17c5f3e2bee8c204660a81e6209208e6eeac8ee5
Parvesh Nigam <mr.nigam.r01@gmail.com>
2026-04-25 15:44:01 +0530

    feat: add error handling middleware and utility classes; integrate morgan for logging

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,50 @@
 import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import ApiError from './utils/apiError.js';
+import errorHandler from './middlewares/errorHandler.js';
 
 
 const app = express();
+
+
+// dev logger
+if(process.env.NODE_ENV === "development"){
+   app.use(morgan("dev"));
+}
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN, 
+    credentials: true 
+}));
+
+app.use(express.urlencoded({
+    extended: true,
+    limit: "16kb"
+}));
+
+app.use(express.json({limit: "16kb"}));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+
+
+// import routes
+
+
+
+
+// use routes
+
+
+
+
+app.use((req,res,next)=>{
+    next(new ApiError(404, "Route not Found"));
+});
+
+app.use(errorHandler);
 
 
 export default app;
---
+++ backend/src/utils/apiResponse.js
@@ -0,0 +1,19 @@
+class ApiResponse{
+    constructor(
+        statusCode,
+        data,
+        message = "Success",
+        meta = {},
+    ){
+
+        this.statusCode = statusCode;
+        this.data = data;
+        this.message = message;
+        this.meta = meta;
+        this.success = statusCode < 400;
+
+    }
+};
+
+
+export default apiResponse;
